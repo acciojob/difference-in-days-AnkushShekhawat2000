3f0a6d7c675c218cb3e41d3c40fab49c8b6b6502
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-04 23:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var dateDiffInDays = function (date1, date2) {
- 
-let y1 = new Date(date).getTime();
-let y2 = new Date(date2).getTime();
-	let diff =  y2 - y1;
+  let y1 = new Date(date1).getTime();  // Corrected variable name and date parsing
+  let y2 = new Date(date2).getTime();
+  let diff = y2 - y1;
 
-	return Math.floor(diff/(1000*60*60*24));
+  return Math.floor(diff / (1000 * 60 * 60 * 24));
 };
 
 // Do not change the code below.
